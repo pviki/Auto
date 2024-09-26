39c5fbbee37b662547ffdb1a01e73c6f11e69add
pviki <pappv@kkszki.hu>
2024-09-26 11:01:59 +0200

    Project changed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoSale.Model;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,44 @@ using System.Threading.Tasks;
 
 namespace AutoSale
 {
-    internal class Program
+    public class Program
     {
 
         public static Connect conn = new Connect();
+        public static List<Car> cars = new List<Car>();
+
+        public static void feladat1()
+        {
+            
+            string sql = "SELECT * FROM cars";
+            conn.Connection.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            do
+            {
+                Car car = new Car();
+
+                car.Id = dr.GetInt32(0);
+                car.Brand = dr.GetString(1);
+                car.Type = dr.GetString(2);
+                car.License = dr.GetString(3);
+                car.Date = dr.GetInt32(4);
+
+                cars.Add(car);
+            } while (dr.Read());
+            
+            conn.Connection.Close();
+        }
         static void Main(string[] args)
         {
+            feladat1();
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"Márka: {item.Brand}, Azonosító: {item.License}");
+            }
         }
     }
 }
