7050056656560a53a8b80f191758ee1a6a1c8bfc
pviki <pappv@kkszki.hu>
2024-09-26 10:19:01 +0200

    Ready to use.

---
+++ Connect.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host= "127.0.0.1";
+            Database = "auto";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User
+                + ";PASSWORD=" + Password + ";SslMode=None";
+
+
+            Connection = new MySqlConnection(ConnectionString);
+
+        }
+    }
+}
